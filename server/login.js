d76f8033089edf9f626be36fcf5ca1e6e56303ca
Abhijit-Sahoo <abhijit09040@gmail.com>
2025-05-05 13:56:16 +0530

    Update login.js

--- server/login.js
+++ server/login.js
@@ -6,7 +6,7 @@
  const app = express();
  app.use(express.json());
  app.use(cors());
- mongoose.connect("mongodb://127.0.0.1:27017/student_db")
+ mongoose.connect("mongo_url")
      .then(() => console.log('Connected to MongoDB'))
      .catch(err => console.error('MongoDB connection error:', err));
      
